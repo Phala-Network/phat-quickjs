d9c14b86d4f458b1503f82581b5a04b35d853885
Kevin Wang <wy721@qq.com>
2022-12-15 03:29:08 +0000

    Better API shape

--- lib.rs
+++ lib.rs
@@ -11,8 +11,14 @@ mod polyfill {
 
     #[no_mangle]
     extern "C" fn __pink_fd_write(fd: c_int, buf: *const c_uchar, len: usize) -> usize {
+        // TODO: a more robust implementation.
         let bin = unsafe { core::slice::from_raw_parts(buf, len) };
-        let message = core::str::from_utf8(bin).unwrap_or("<Invalid UTF-8 string>");
+        let message = core::str::from_utf8(bin)
+            .unwrap_or("<Invalid UTF-8 string>")
+            .trim_end();
+        if message.is_empty() {
+            return len;
+        }
         match fd {
             1 => info!("JS: {}", message),
             2 => error!("JS: {}", message),
@@ -39,10 +45,9 @@ mod qjs {
         }
 
         #[ink(message)]
-        pub fn eval(&self, js: String) -> bool {
+        pub fn eval(&self, js: String) -> Result<String, String> {
             info!("evaluating js [{js}]");
-            let rv = qjs_sys::eval(&js);
-            rv == 0
+            qjs_sys::eval(&js)
         }
     }
 }
