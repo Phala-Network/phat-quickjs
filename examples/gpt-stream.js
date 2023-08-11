deeb393ee58680f6368cc7befb0827912ecd5713
Kevin Wang <wy721@qq.com>
2023-08-11 06:41:15 +0000

    Refine the example

--- examples/gpt-stream.js
+++ examples/gpt-stream.js
@@ -1,34 +1,28 @@
 const OPENAI_API_KEY = "";
 
-function encode(o) {
-  return new TextEncoder().encode(JSON.stringify(o))
-}
-
-const headers = {
-  'Accept': 'application/json, text/plain, */*',
-  'Content-Type': 'application/json',
-  'Authorization': `Bearer ${OPENAI_API_KEY}`,
-};
-
-const body = encode(
-  {
-    "model": "gpt-3.5-turbo",
-    "messages": [{ "role": "user", "content": "Tell me a story" }],
-    "stream": true
-  }
-);
-
 async function main() {
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: 'POST',
-    headers,
-    body
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${OPENAI_API_KEY}`,
+    },
+    body: encode({
+      "model": "gpt-3.5-turbo",
+      "messages": [{ "role": "user", "content": "Tell me a story" }],
+      "stream": true
+    })
   });
+
   // TODO: this should be wrapped in a line-based reader
   for await (const chunk of response.body) {
     console.log('chunk:', new TextDecoder().decode(chunk));
   }
 }
 
-main().catch(console.error)
+function encode(o) {
+  return new TextEncoder().encode(JSON.stringify(o))
+}
 
+main().catch(console.error)
