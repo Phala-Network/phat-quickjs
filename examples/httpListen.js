a04c2af795c577ed3f128b0198df86421c340904
Kevin Wang <wy721@qq.com>
2023-08-24 16:24:04 +0000

    async response example

--- examples/httpListen.js
+++ examples/httpListen.js
@@ -1,6 +1,7 @@
 console.log = Sidevm.inspect;
 console.log('Start to listen http requests...');
-Sidevm.httpListen((req) => {
+
+Sidevm.httpListen(async (req) => {
     console.log('Incomming HTTP request:', req);
     Sidevm.httpSendResponse(req.opaqueResponseTx, {
         status: 200,
@@ -9,26 +10,37 @@ Sidevm.httpListen((req) => {
             'X-Foo': 'Bar',
         }
     })
-    let chunks = [
-        "Hello, \n",
-        "World!\n",
-        "This is a test.\n",
-    ];
     const writer = Sidevm.httpMakeWriter(req.opaqueOutputStream);
-    const marker2 = Sidevm.marker("marker2");
-    const p = {
-        writeAll(data) {
-            console.log('marker2', marker2);
-            const chunk = new TextEncoder().encode(data[0]);
-            Sidevm.httpWriteChunk(writer, chunk, (suc, err) => {
-                const rest = data.slice(1);
-                if (rest.length > 0) {
-                    setTimeout(p.writeAll, 1000, rest);
-                } else {
-                    p.writeAll = () => { };
-                }
-            });
-        }
-    };
-    p.writeAll(chunks);
+    await writeString(writer, `method: ${req.method}\n`);
+    await sleep(1000);
+    await writeString(writer, `path: ${req.path}\n`);
+    await sleep(1000);
+    await writeString(writer, `query: ${req.query}\n`);
+    await sleep(1000);
+    await writeString(writer, `headers: \n`);
+    for (var p of Object.entries(req.headers)) {
+        await writeString(writer, `    ${p[0]}: ${p[1]}\n`);
+        await sleep(500);
+    }
 });
+
+
+
+async function writeString(writer, s) {
+    const data = new TextEncoder().encode(s);
+    return new Promise((resolve, reject) => {
+        Sidevm.httpWriteChunk(writer, data, (suc, err) => {
+            if (suc) {
+                resolve();
+            } else {
+                reject(err);
+            }
+        });
+    });
+}
+
+async function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
