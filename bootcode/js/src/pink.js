5fc2bdd890f9cfb7d46106eb4c26e6ba10e45e41
Kevin Wang <wy721@qq.com>
2023-09-06 12:10:01 +0000

    Fix console.log

--- bootcode/js/src/pink.js
+++ bootcode/js/src/pink.js
@@ -1,38 +1,17 @@
 (function (g) {
-    function toB(v) {
-        if (typeof v != 'string') {
-            v = new Uint8Array(v);
+    g.console = {
+        log(...args) {
+            pink.log(1, args);
+        },
+        info(...args) {
+            pink.log(2, args);
+        },
+        warn(...args) {
+            pink.log(3, args);
+        },
+        error(...args) {
+            pink.log(4, args);
         }
-        return v;
     }
-    g.pink = {
-        invokeContract(c) {
-            return __hostCall(0, {
-                callee: toB(c.callee),
-                selector: c.selector,
-                input: toB(c.input),
-                gasLimit: c.gasLimit || 0,
-                value: c.value || 0,
-                allowReentry: c.allowReentry || false,
-            });
-        },
-        invokeContractDelegate(c) {
-            return __hostCall(1, {
-                codeHash: toB(c.codeHash),
-                selector: c.selector,
-                input: toB(c.input),
-                allowReentry: c.allowReentry || false,
-            });
-        },
-        httpRequest(c) {
-            return __hostCall(2, c);
-        },
-        batchHttpRequest(c, t) {
-            return __hostCall(3, c, t || 10000);
-        },
-        hash(alg, m) {
-            return __hostCall(5, alg, m);
-        },
-    };
 }(globalThis))
 export default {};
