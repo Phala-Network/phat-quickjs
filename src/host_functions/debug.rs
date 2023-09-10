1034b79a24dcd56c61d430033fb0b01925968886
Kevin Wang <wy721@qq.com>
2023-09-10 12:12:55 +0000

    Add sidevm repr

--- src/host_functions/debug.rs
+++ src/host_functions/debug.rs
@@ -3,6 +3,7 @@ use log::info;
 
 pub(crate) fn setup(ns: &js::Value) -> Result<()> {
     ns.define_property_fn("marker", marker)?;
+    ns.define_property_fn("repr", to_debug_representation)?;
     Ok(())
 }
 
@@ -18,4 +19,12 @@ impl Drop for Marker {
 #[js::host_call(with_context)]
 fn marker(service: ServiceRef, _this: js::Value, tag: String) -> js::Value {
     js::Value::new_opaque_object(service.raw_ctx(), Marker { tag })
+}
+
+#[js::host_call]
+fn to_debug_representation(obj: js::Value, level: Option<u8>) -> Result<String> {
+    let level = level.unwrap_or(5);
+    let mut buf = String::new();
+    js::recursive_to_string(&obj, level, true, &mut buf);
+    Ok(buf)
 }
