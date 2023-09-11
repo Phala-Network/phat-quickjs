a8ea967ce57ae4d80b857e96ec06608f4e267c11
Kevin Wang <wy721@qq.com>
2023-09-11 01:00:05 +0000

    Update qjsbind

--- host_functions.rs
+++ host_functions.rs
@@ -69,7 +69,7 @@ fn setup_encoding_functions(pink: &js::Value, ctx: &js::Context) -> js::Result<(
     pink.define_property_fn("hexEncode", ext::hex::encode)?;
     pink.define_property_fn("hexDecode", ext::hex::decode)?;
     let scale = ctx.new_object();
-    ext::scale2::setup(&scale)?;
+    ext::scale2::setup(&scale, ctx)?;
     pink.set_property("SCALE", &scale)?;
     Ok(())
 }
