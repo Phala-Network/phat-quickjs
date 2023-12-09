a0a64ee7b2da9cba34b89a6da33037dfc7687f4c
Kevin Wang <wy721@qq.com>
2023-12-09 02:30:32 +0000

    hash: Use BytesOrString

--- src/host_functions/hash.rs
+++ src/host_functions/hash.rs
@@ -18,13 +18,13 @@ fn do_hash<T: Digest>(data: impl AsRef<[u8]>) -> Vec<u8> {
 }
 
 #[js::host_call]
-fn hash(algorithm: js::JsString, message: AsBytes<Vec<u8>>) -> Result<AsBytes<Vec<u8>>> {
+fn hash(algorithm: js::JsString, message: js::BytesOrString) -> Result<AsBytes<Vec<u8>>> {
     let hash = match algorithm.as_str() {
-        "sha256" => do_hash::<sha2::Sha256>(message.0),
-        "keccak256" => do_hash::<sha3::Keccak256>(message.0),
-        "blake2b128" => do_hash::<Blake2b<U16>>(message.0),
-        "blake2b256" => do_hash::<Blake2b<U32>>(message.0),
-        "blake2b512" => do_hash::<Blake2b<U64>>(message.0),
+        "sha256" => do_hash::<sha2::Sha256>(message),
+        "keccak256" => do_hash::<sha3::Keccak256>(message),
+        "blake2b128" => do_hash::<Blake2b<U16>>(message),
+        "blake2b256" => do_hash::<Blake2b<U32>>(message),
+        "blake2b512" => do_hash::<Blake2b<U64>>(message),
         _ => bail!("Unsupported hash algorithm: {}", algorithm.as_str()),
     };
     Ok(hash.into())
