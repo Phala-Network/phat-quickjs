8e74ae06f4a1f2c06b7c4d234ec896c57c57098c
Kevin Wang <wy721@qq.com>
2024-05-31 11:58:37 +0800

    langchain pipe: prompt->model->stringParser

--- WapoJS/examples/langchain/src/index.ts
+++ WapoJS/examples/langchain/src/index.ts
@@ -1,4 +1,6 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 const OPENAI_API_KEY = process.env.GPT_API_KEY;
 const BASE_URL = process.env.GPT_BASE_URL || "https://api.openai.com";
@@ -9,17 +11,24 @@ async function main() {
         return;
     }
     console.log("BASE_URL:", BASE_URL);
+
+    const prompt = ChatPromptTemplate.fromMessages([
+        ["system", "You are a world class technical documentation writer."],
+        ["user", "{input}"],
+    ]);
     const chatModel = new ChatOpenAI({
         openAIApiKey: OPENAI_API_KEY,
         configuration: {
             baseURL: BASE_URL,
         }
     });
-    const stream = await chatModel.stream("Hello! Tell me about yourself.");
+    const outputParser = new StringOutputParser();
+    const llmChain = prompt.pipe(chatModel).pipe(outputParser);
+    const stream = await llmChain.stream({ input: "Hello! Tell me about yourself." });
     const chunks = [];
     for await (const chunk of stream) {
         chunks.push(chunk);
-        console.log(`${chunk.content}|`);
+        console.log(`${chunk}|`);
     }
 }
 
