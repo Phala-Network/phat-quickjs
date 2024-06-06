4db99d3f75d9be19fa1c209ef36cc3e8e14d7f00
Kevin Wang <wy721@qq.com>
2024-06-06 17:26:20 +0800

    Update polkadot demo

--- WapoJS/examples/polkadotjs/src/index.ts
+++ WapoJS/examples/polkadotjs/src/index.ts
@@ -7,7 +7,7 @@ async function main() {
     await waitReady();
     console.log('crypto ready');
 
-    /// Keyring
+    // Keyring
     const keyring = new Keyring({ type: 'sr25519' });
     const alice = keyring.addFromUri('//Alice', { name: 'Alice default' });
     console.log(`${alice.meta.name}: has address ${alice.address} with publicKey [${alice.publicKey}]`);
@@ -16,13 +16,20 @@ async function main() {
     const isValid = alice.verify(message, signature, alice.publicKey);
     console.log(`The signature ${u8aToHex(signature)}, is ${isValid ? '' : 'in'}valid`);
 
-    /// RPC
+    // RPC
     const url = 'wss://poc6.phala.network/ws';
     const provider = new WsProvider(url);
     console.log(`connecting to ${url}`);
     const api = await ApiPromise.create({ provider });
     console.log('genesis hash:', api.genesisHash.toHex());
     console.log('ed:', api.consts.balances.existentialDeposit.toJSON());
+
+    // Transcation(remark)
+    const nonce = await api.rpc.system.accountNextIndex(alice.address);
+    console.log(`nonce is ${nonce}`);
+    const remark = api.tx.system.remarkWithEvent('Hello, WapoJS!');
+    const tx = await remark.signAndSend(alice, { nonce });
+    console.log(`tx hash is ${tx.toHuman()}`);
 }
 
 main().catch(console.error).finally(() => process.exit());
