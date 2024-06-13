347c1805816f6918ae1743f8876365c2dd3d9579
tolak <kalot.wang@gmail.com>
2024-06-13 22:33:39 +0800

    wip: run zama-tfhe in quickjs

---
+++ WapoJS/examples/zama-tfhe/src/index.ts
@@ -0,0 +1,45 @@
+import {
+    CompactFheUint8List,
+    TfheCompactPublicKey,
+    TfheConfigBuilder,
+    TfheClientKey,
+    ShortintParameters,
+    ShortintParametersName,
+} from "node-tfhe";
+
+function createTfheKeypair() {
+    const block_params = new ShortintParameters(
+      ShortintParametersName.PARAM_MESSAGE_2_CARRY_2_COMPACT_PK_PBS_KS,
+    );
+    const config = TfheConfigBuilder.default()
+      .use_custom_parameters(block_params)
+      .build();
+    const clientKey = TfheClientKey.generate(config);
+    let publicKey = TfheCompactPublicKey.new(clientKey);
+    publicKey = TfheCompactPublicKey.deserialize(publicKey.serialize());
+    return { clientKey, publicKey };
+};
+
+function encryptUint8(value: number, pubkey: TfheCompactPublicKey): Uint8Array {
+    const uint8Array = new Uint8Array([value]);
+    const encrypted = CompactFheUint8List.encrypt_with_compact_public_key(
+      uint8Array,
+      pubkey,
+    );
+    return encrypted.serialize();
+};
+
+function main() {
+    const { clientKey, publicKey } = createTfheKeypair();
+    const value = 100;
+    const encrypted = encryptUint8(value, publicKey);
+    console.log(`Encrypted: ${encrypted}`);
+    const compactList = CompactFheUint8List.deserialize(encrypted);
+    let encryptedList = compactList.expand();
+    encryptedList.forEach((v) => {
+      const decrypted = v.decrypt(clientKey);
+      console.log(`Decrypted: ${decrypted}`);
+    });
+}
+
+main();
---
+++ WapoJS/examples/zama-tfhe/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'production',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      "util": require.resolve("util/"),
+      "path": require.resolve("path-browserify"),
+      "fs": false
+    }
+  },
+  target: 'web',
+  devtool: 'source-map',
+};
